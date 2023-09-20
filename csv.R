5f8661a43c5274dd5d93e843791da88afeab6d87
Angel de la Nava <a.nava1@usp.ceu.es>
2023-09-20 18:27:49 +0200

    Code refactored

---
+++ csv.R
@@ -0,0 +1,16 @@
+csvSum <- function(A, B){
+  return (c(A, B))
+}
+
+csvTime <- function(df, fileName){
+  write.csv(df, fileName)
+}
+csvFreq <- function(df, fileName){
+  write.csv(df, fileName)
+}
+csvCorr <- function(df, fileName){
+  write.csv(df, fileName)
+}
+csvLya <- function(df, fileName){
+  write.csv(df, fileName)
+}
